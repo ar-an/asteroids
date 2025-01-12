af319363e2beed722a5f43083dd5365684fe5a44
aran <aristotelis_anagnostou@yahoo.gr>
2025-01-12 19:20:57 +0200

    spawning asteroids

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,15 +10,26 @@ def main():
     clock = pygame.time.Clock()
     dt: int = 0
 
-    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
+
+    Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    AsteroidField()
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for draws in drawables:
+            draws.draw(screen)
+        for updates in updatables:
+            updates.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1_000
 
